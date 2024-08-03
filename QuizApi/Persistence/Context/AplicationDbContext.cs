78ffe10626bb0595926c5fccfcd3b153ea571079
Frans <frans_421_12@hotmail.com>
2024-08-03 00:01:07 -0500

    feat: DbContext and EF

---
+++ QuizApi/Persistence/Context/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApi.Domain.Models;
+
+namespace QuizApi.Persistence.Context
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
