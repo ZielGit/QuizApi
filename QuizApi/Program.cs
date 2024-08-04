e4a2c7efe46820b66bdac44b2206c4d75cb125da
Frans <frans_421_12@hotmail.com>
2024-08-04 14:14:37 -0500

    feat:  Init Connection and Migration

--- QuizApi/Program.cs
+++ QuizApi/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using QuizApi.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("Conexion");
+builder.Services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
