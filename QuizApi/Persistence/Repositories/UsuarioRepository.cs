dd5b03a308591691bb64a92b7811ca466d36dd80
Frans <frans_421_12@hotmail.com>
2024-08-04 19:27:57 -0500

    feat: SaveUser Repository

--- QuizApi/Persistence/Repositories/UsuarioRepository.cs
+++ QuizApi/Persistence/Repositories/UsuarioRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace QuizApi.Persistence.Repositories
+﻿using QuizApi.Domain.IRepositories;
+using QuizApi.Domain.Models;
+using QuizApi.Persistence.Context;
+
+namespace QuizApi.Persistence.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository: IUsuarioRepository
     {
+        private readonly AplicationDbContext _context;
+        public UsuarioRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveUser(Usuario usuario)
+        {
+            _context.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
     }
 }
