90aa5d7e2604b8ef721246fe34c70b21795e0e67
Frans <frans_421_12@hotmail.com>
2024-08-04 19:29:13 -0500

    feat: AddScoped User

--- QuizApi/Program.cs
+++ QuizApi/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using QuizApi.Domain.IRepositories;
 using QuizApi.Persistence.Context;
+using QuizApi.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("Conexion");
 builder.Services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
