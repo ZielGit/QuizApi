52f3b5566b28612ae14d18e51535374e4ffff299
Frans <frans_421_12@hotmail.com>
2024-08-04 19:27:36 -0500

    feat: SaveUser Interface

---
+++ QuizApi/Domain/IRepositories/IUsuarioRepository.cs
@@ -0,0 +1,9 @@
+﻿using QuizApi.Domain.Models;
+
+namespace QuizApi.Domain.IRepositories
+{
+    public interface IUsuarioRepository
+    {
+        Task SaveUser(Usuario usuario);
+    }
+}
---
+++ QuizApi/Domain/IServices/IUsuarioService.cs
@@ -0,0 +1,9 @@
+﻿using QuizApi.Domain.Models;
+
+namespace QuizApi.Domain.IServices
+{
+    public interface IUsuarioService
+    {
+        Task SaveUser(Usuario usuario);
+    }
+}
